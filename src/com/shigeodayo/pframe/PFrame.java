a1687a004572400a0d7166e2d76cb0ca85c9a683
shigeodayo <shigeo.yoshida.shigeo@gmail.com>
2012-04-16 17:13:04 +0900

    fix bugs on windows.

--- src/com/shigeodayo/pframe/PFrame.java
+++ src/com/shigeodayo/pframe/PFrame.java
@@ -10,6 +10,8 @@ public class PFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	private PApplet target = null;
+	
 	/**
 	 * constructor
 	 * 
@@ -21,14 +23,48 @@ public class PFrame extends JFrame {
 	 *            window location
 	 */
 	public PFrame(PApplet target, int x, int y) {
-		setVisible(true);
+		this.target = target;
+		
 		add(target);
 		target.init();
+		setVisible(true);
 		Insets insets = getInsets();
-		setBounds(x, y, target.width + insets.right + insets.left, target.height + insets.top + insets.bottom);
-		setSize(target.width + insets.right + insets.left, target.height + insets.top + insets.bottom);
+		setLocation(x, y);
+		setSize(target.width + insets.right + insets.left, target.height
+				+ insets.top + insets.bottom);
 	}
 
+	/**
+	 * constructor
+	 * 
+	 * @param target
+	 *            target PApplet
+	 * @param x
+	 *            window location
+	 * @param y
+	 *            window location
+	 * @param showTitleBar
+	 *            showing title bar or not
+	 * 
+	 */
+	public PFrame(PApplet target, int x, int y, boolean showTitleBar) {
+		this.target = target;
+
+		if (!showTitleBar) {
+			removeNotify();
+			setUndecorated(true);
+			addNotify();
+		}
+		add(target);
+		target.init();
+		setVisible(true);
+		Insets insets = getInsets();
+		setLocation(x, y);
+		setSize(target.width + insets.right + insets.left, target.height
+				+ insets.top + insets.bottom);
+	}
+
+	
 	/**
 	 * constructor
 	 * 
@@ -42,13 +78,57 @@ public class PFrame extends JFrame {
 	 *            window width
 	 * @param height
 	 *            window height
+	 * 
 	 */
 	public PFrame(PApplet target, int x, int y, int width, int height) {
-		setVisible(true);
+		this.target = target;
+
 		add(target);
 		target.init();
+		setVisible(true);
 		Insets insets = getInsets();
-		//setBounds(x, y, width, height + insets.top);
-		setBounds(x, y, width + insets.right + insets.left, height + insets.top + insets.bottom);
+		setLocation(x, y);
+		setSize(width + insets.right + insets.left, height + insets.top
+				+ insets.bottom);
+
 	}
+	/**
+	 * constructor
+	 * 
+	 * @param traget
+	 *            target PApplet
+	 * @param x
+	 *            window location
+	 * @param y
+	 *            window location
+	 * @param width
+	 *            window width
+	 * @param height
+	 *            window height
+	 * @param showTitleBar
+	 *            showing title bar or not
+	 * 
+	 */
+	public PFrame(PApplet target, int x, int y, int width, int height,
+			boolean showTitleBar) {
+		this.target = target;
+
+		if (!showTitleBar) {
+			removeNotify();
+			setUndecorated(true);
+			addNotify();
+		}
+		add(target);
+		target.init();
+		setVisible(true);
+		Insets insets = getInsets();
+		setLocation(x, y);
+		setSize(width + insets.right + insets.left, height + insets.top
+				+ insets.bottom);
+
+	}
+	
+	public PApplet getApplet() {
+		return target;
+	}	
 }
